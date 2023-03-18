b32821948cbecfe81872bcb59fc496ad1f55e7c5
Levin Du <zslevin@gmail.com>
2023-03-18 15:55:10 +0800

    add various blockdiag commands support

--- ob-blockdiag.el
+++ ob-blockdiag.el
@@ -33,21 +33,9 @@
 ;;; Code:
 (require 'ob)
 
-(defvar org-babel-default-header-args:blockdiag
-  '(
-    (:results . "file")
-    (:exports . "results")
-    (:tool    . "blockdiag3")
-    (:transparency . nil)
-    (:antialias . nil)
-    (:font    . nil)
-    (:size    . nil)
-    (:type    . nil))
-  "Default arguments for drawing a blockdiag image.")
+;; (add-to-list 'org-src-lang-modes '("blockdiag" . blockdiag))
 
-(add-to-list 'org-src-lang-modes '("blockdiag" . blockdiag))
-
-(defun org-babel-execute:blockdiag (body params)
+(defun org-babel-execute:_blockdiag (body params)
   (let ((file (cdr (assoc :file params)))
         (tool (cdr (assoc :tool params)))
         (transparency (cdr (assoc :transparency params)))
@@ -58,6 +46,7 @@
 
         (buffer-name "*ob-blockdiag*")
         (error-template "Subprocess '%s' exited with code '%d', see output in '%s' buffer"))
+    (unless file (error "no :file specified"))
     (save-window-excursion
       (let ((buffer (get-buffer buffer-name)))(if buffer (kill-buffer buffer-name) nil))
       (let ((data-file (org-babel-temp-file "blockdiag-input"))
@@ -72,6 +61,48 @@
         (let
             ((exit-code (apply 'call-process tool nil buffer nil (append args (list data-file)))))
           (if (= 0 exit-code) nil (error (format error-template tool exit-code buffer-name))))))))
+
+(defcustom org-babel-blockdiag-commands
+  '("blockdiag" "actdiag" "seqdiag" "nwdiag" "rackdiag" "packetdiag")
+  "List of names of blockdiag commands supported by babel blockdiag code blocks.
+Call `org-babel-blockdiag-initialize' when modifying this variable
+outside the Customize interface."
+  :group 'org-babel
+  :type '(repeat (string :tag "Blockdiag command: "))
+  :set (lambda (symbol value)
+         (set-default-toplevel-value symbol value)
+         (org-babel-blockdiag-initialize)))
+
+(defun org-babel-blockdiag-initialize ()
+  "Define execution functions associated to blockdiag commands.
+This function has to be called whenever `org-babel-blockdiag-commands'
+is modified outside the Customize interface."
+  (interactive)
+  (dolist (name org-babel-blockdiag-commands)
+    (let ((fname (intern (concat "org-babel-execute:" name)))
+          (exe (or (executable-find name)
+                   (executable-find (concat name "3"))
+                   name)))
+      ;; org-babel-execute:?
+      (defalias fname
+        (lambda (body params)
+          ;; (:documentation
+          ;;  (format "Execute a block of %s command with Babel." name))
+          (org-babel-execute:_blockdiag body params)))
+      (put fname 'definition-name 'org-babel-blockdiag-initialize)
+      ;; org-babel-default-header-args:?
+      (funcall (if (fboundp 'defvar-1) #'defvar-1 #'set) ;Emacs-29
+               (intern (concat "org-babel-default-header-args:" name))
+               `(
+                 (:results . "file")
+                 (:exports . "results")
+                 (:tool    . ,exe)
+                 (:transparency . nil)
+                 (:antialias . nil)
+                 (:font    . nil)
+                 (:size    . nil)
+                 (:type    . nil))
+               nil))))
 
 (provide 'ob-blockdiag)
 ;;; ob-blockdiag.el ends here
